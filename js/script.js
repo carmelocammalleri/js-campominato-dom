14a7043ab22e817702748744313eddf35cf15b4e
Melozzo <carmecamma1995@gmail.com>
2023-09-19 16:56:13 +0200

    [fix]: try to find bugs

--- js/script.js
+++ js/script.js
@@ -16,47 +16,46 @@ function addBoxes (numBoxes){
 
     //4.
     box.addEventListener('click', function() {
-
-    //3.2 veriffica click cella con bomba 
       let isBomb = false;
-      if (listBomb.includes(i)){
+      if (listBom.includes(i)){
         isBomb = true;
       }
 
       if(isBomb){
         this.classList.toggle('bomb');
-
       }else {
       this.classList.toggle('clicked');
       }
+    
+      console.log(box);
+    //3.
   })
   }
 }
  
 //5.
-let listBomb =[];
+let listBom =[];
 
 btnStart.addEventListener('click', function(){
   containerGame.classList.remove('hide')
   containerGame.innerHTML= ' ';
-  // 2.2 generazione array con numeri randomici all'interno
 
+  // 3. creazione
   for(let i = 0 ; i < 16; i++){ 
-    let tmp = Math.floor(Math.random() * 49) +1;   
+    let tmp = Math.floor(Math.random() * 49 ) +1;
+    
     let nuovo = true;
-  
     for(let j = 0; j < i; j++){
-      if(listBomb[j]==tmp)nuovo=false;
+      if(listBom[j]==tmp)nuovo=false;
     }
     if(nuovo){
-      listBomb[i]=tmp;
+      listBom[i]=tmp;
     }else{
       i--;
     }
+  } 
   lvl();
-  console.log(listBomb)
-}
-
+  console.log(listBom)
   
 })
 
